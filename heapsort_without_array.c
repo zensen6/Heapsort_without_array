32179d4d2c1e37704f6a7eb9dfa38f2f4e42da31
junhee Kim <42162320+zensen6@users.noreply.github.com>
2019-05-28 09:31:40 +0900

    Update heapsort_without_array.c

--- heapsort_without_array.c
+++ heapsort_without_array.c
@@ -1,99 +1,397 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define MIN -1000000000
 
-const int MAX = 1000000;
+int count = 0;
+int last;
+int existflag = 0;
 
-int check[1000000] = { 0, };
 
-typedef struct node{
-	int data;
-	struct node *left;
-	struct node *right;
+typedef struct node {
+
+	int key;
+	struct node* parent;
+	struct node* leftChild;
+	struct node* rightChild;
+
 }Node;
- 
-Node* newNode(int data){
-	Node* temp = (Node*)malloc(sizeof(Node));
-	temp->data = data;
-	temp->left = temp->right = NULL;
-	return temp;
+
+
+
+typedef struct Node *treePointer;
+
+
+
+Node* root;
+
+Node* searching;
+
+
+
+void swap(Node* a, Node* b) {
+
+
+
+	int tmp = a->key;
+
+	a->key = b->key;
+
+	b->key = tmp;
+
+	return;
+
+
+
 }
 
-Node* constructTreeUtil(int *pre, int* preIndex, int low, int high, int size){
-	  
-	if (*preIndex >= size || low > high)
-		return NULL;
 
-	Node* root = newNode(pre[*preIndex]);
-	*preIndex = *preIndex + 1;
 
-  
-	if (low == high)
-		return root;
+void init() {
 
-  
-	int i;
-	for (i = low; i <= high; ++i)
-		if (pre[i] > root->data)
-			break;
 
- 
-	root->left = constructTreeUtil(pre, preIndex, *preIndex, i - 1, size);
-	root->right = constructTreeUtil(pre, preIndex, i, high, size);
 
-	return root;
-}
- 
-Node *constructTree(int pre[], int size){
-	int preIndex = 0;
-	return constructTreeUtil(pre, &preIndex, 0, size - 1, size);
+	root = (Node*)malloc(sizeof(Node));
+
+	root->key = MIN;
+
+	root->leftChild = NULL;
+
+	root->rightChild = NULL;
+
+	root->parent = NULL;
+
 }
 
- 
-void printInorder(Node* node){
-	if (node == NULL)
+
+
+int maximun(int a, int b) {
+
+	return a > b ? a : b;
+
+}
+
+
+
+int smallbin(int x) {
+
+
+
+	int sum = 0;
+
+	int i = 1;
+
+	while (sum <= x) {
+
+		sum += i;
+		i = i << 1;
+	}
+
+	return sum - (i >> 1);
+
+}
+
+
+
+int largebin(int x) {
+
+
+
+	int sum = 0;
+
+	int i = 1;
+
+	while (sum < x) {
+
+		sum += i;
+
+		i = i << 1;
+	}
+
+	return sum;
+
+}
+
+
+
+void insertion(int key) {
+
+	if (count==0) {
+
+		root->key = key;
+		count = 1;
 		return;
-	printInorder(node->left);
 
-	printf("%d ", node->data);
+	}
 
-	printInorder(node->right);
-}
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->key = key;
+	node->leftChild = NULL;
+	node->rightChild = NULL;
+	node->parent = NULL;
+	count += 1;
+	Node* search = root;
+	Node* cur = root;
+	int start = smallbin(count - 1) + 1;
+	int end = largebin(count);
 
-void printPostorder(Node* node) {
+	while (start <= end) {
+		int mid = (start + end) / 2;
 
-	if (node == NULL)
-		return;
-	printPostorder(node->left);
+		if (count <= mid) {
 
-	printPostorder(node->right);
+			if (cur->leftChild == NULL) {
 
-	printf("%d ",node->data);
-}
+				cur->leftChild = node;
+				node->parent = cur;
+				search = node;
+				break;
+			}
 
- 
-int main(){
-	int n;
-	scanf("%d", &n);
-	int* pre = (int*)malloc(n * sizeof(int));
-	int size = n;
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &pre[i]);
-		if (!check[pre[i]]) check[pre[i]] = 1;
+			else cur = cur->leftChild;
+
+			end = mid - 1;
+
+		}
+
 		else {
 
-			printf("cannot construct BST\n");
-			return 0;
+			if (cur->rightChild == NULL) {
+
+				cur->rightChild = node;
+				node->parent = cur;
+				search = node;
+				break;
+
+			}
+
+			else cur = cur->rightChild;
+			start = mid + 1;
+
+		}
+
+	}
+
+	//printf("%d\n", search->parent->key);
+	
+	while (search->parent != NULL) {
+
+		if (search->key > search->parent->key) {
+
+			swap(search, search->parent);
+
+			search = search->parent;
+		}
+		else {
+			break;
 
 		}
 	}
-	Node *root = constructTree(pre, size);
+	
+	return;
 
-	printf("Inorder: ");
-	printInorder(root);
-	printf("\n");
-	printf("Postorder: ");
-	printPostorder(root);
-	free(pre);
-	return 0;
 }
+
+
+
+void search() {
+
+	searching = root;
+	int start = smallbin(count - 1) + 1;
+	int end = largebin(count);
+
+	while (start <= end) {
+
+		int mid = (start + end) / 2;
+
+		if (count <= mid) {
+
+			searching = searching->leftChild;
+			if (searching->leftChild == NULL && searching->rightChild == NULL) break;
+			end = mid - 1;
+
+		}
+
+		else {
+
+			searching = searching->rightChild;
+			if (searching->leftChild == NULL && searching->rightChild == NULL) break;
+			start = mid + 1;
+		}
+
+
+	}
+
+}
+
+
+
+int deletion() {
+
+	if (count==0) {
+
+		return -1;
+	}
+
+	int rootnum = root->key;
+
+	if (count == 1) {
+
+		count = 0;
+		root->key = MIN;
+		return rootnum;
+
+	}
+
+	Node* cur;
+	search();
+	cur = searching;
+	last = cur->key;
+
+	if (last == cur->parent->leftChild->key) {
+
+		cur->parent->leftChild = NULL;
+
+	}
+
+	else {
+
+		cur->parent->rightChild = NULL;
+
+	}
+
+	free(cur);
+
+	count--;
+	root->key = last;
+	Node* pcur = root;
+
+	while (!(pcur->leftChild == NULL && pcur->rightChild == NULL)) {
+
+		if (pcur->leftChild != NULL && pcur->leftChild->key > pcur->key) {
+
+			if (pcur->rightChild != NULL && pcur->leftChild->key > pcur->rightChild->key) {
+
+				swap(pcur, pcur->leftChild);
+				pcur = pcur->leftChild;
+
+			}
+
+			else if (pcur->rightChild != NULL && pcur->leftChild->key < pcur->rightChild->key) {
+
+				swap(pcur, pcur->rightChild);
+				pcur = pcur->rightChild;
+			}
+
+			else {
+
+				swap(pcur, pcur->leftChild);
+				pcur = pcur->leftChild;
+			}
+
+		}
+
+		else if (pcur->rightChild != NULL && pcur->rightChild->key > pcur->key) {
+
+			if (pcur->leftChild != NULL && pcur->leftChild->key < pcur->rightChild->key) {
+
+				swap(pcur, pcur->rightChild);
+				pcur = pcur->rightChild;
+
+			}
+
+			else if (pcur->leftChild != NULL && pcur->leftChild->key > pcur->rightChild->key) {
+
+				swap(pcur, pcur->leftChild);
+				pcur = pcur->leftChild;
+			}
+
+			else {
+
+				swap(pcur, pcur->rightChild);
+				pcur = pcur->rightChild;
+			}
+
+		}
+
+		else break;
+	}
+	return rootnum;
+}
+
+
+
+void printing(Node* rt) {
+
+	if (rt == NULL) return;
+	printing(rt->leftChild);
+	printf("%d ", rt->key);
+	printing(rt->rightChild);
+}
+
+
+
+int existnumber(int num, Node* rt) {
+
+	if (rt == NULL) return;
+	if (num == rt->key) {
+
+		printf("Exist number\n");
+		existflag = 1;
+		return;
+
+	}
+
+	else {
+
+		existnumber(num, rt->leftChild);
+		existnumber(num, rt->rightChild);
+	}
+
+}
+
+
+
+int main() {
+
+	init();
+	
+	char c;
+	int d;
+	do {
+		existflag = 0;
+		scanf("%c", &c);
+		if (c == 'i') {
+			//getchar();
+
+			scanf("%d", &d);
+			existnumber(d, root);
+			if (existflag) {}
+
+			else {
+
+				printf("Insert %d\n", d);
+
+				insertion(d);
+
+			}
+
+		}
+
+		else if (c == 'd') {
+			getchar();
+
+			if (root->key == MIN) {
+
+				printf("The heap is empty\n");
+			}
+
+			else printf("Delete %d\n", deletion());
+		}
+
+	} while (c != 'q');
+	
+
+	return 0;
+
+}
+
